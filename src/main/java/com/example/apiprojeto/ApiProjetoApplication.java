736efe0c4ece6e1f8c321fb13910ba5ee60b926d
giovaner10 <giovanerr10@gmail.com>
2022-04-27 12:36:51 -0300

    Initial commit

---
+++ src/main/java/com/example/apiprojeto/ApiProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjetoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiProjetoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apiprojeto/ApiProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjetoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
