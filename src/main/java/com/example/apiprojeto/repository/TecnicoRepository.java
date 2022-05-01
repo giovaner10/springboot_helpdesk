7ee4d20e3968e62d373c5c7c137d548012525e4b
giovaner10 <giovanerr10@gmail.com>
2022-04-30 22:11:04 -0300

    tecnico repository

--- src/main/java/com/example/apiprojeto/repository/TecnicoRepository.java
+++ src/main/java/com/example/apiprojeto/repository/TecnicoRepository.java
@@ -1,6 +1,5 @@
 package com.example.apiprojeto.repository;
 
-import com.example.apiprojeto.domain.Pessoa;
 import com.example.apiprojeto.domain.Tecnico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
