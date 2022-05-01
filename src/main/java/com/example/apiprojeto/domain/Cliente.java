21cfb40d1735c8512a6d4c6ecbe721c072db71e1
giovaner10 <giovanerr10@gmail.com>
2022-04-30 22:13:35 -0300

    cliente

--- src/main/java/com/example/apiprojeto/domain/Cliente.java
+++ src/main/java/com/example/apiprojeto/domain/Cliente.java
@@ -1,37 +1,101 @@
 package com.example.apiprojeto.domain;
 
-
 import com.example.apiprojeto.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
-public class Cliente extends Pessoa {
-    private static final long serialVersionUID = 1L;
+public class Cliente {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String senha;
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<Integer> perfis = new HashSet<>();
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataCriacao = LocalDate.now();
 
 
-    @OneToMany(mappedBy = "cliente")
-    private List<Chamados> chamados = new ArrayList();
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
-        super(id, nome, cpf, email, senha);
-        addPerfil(Perfil.CLIENTE);
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+        setPerfis(Perfil.CLIENTE);
 
     }
 
     public Cliente() {
-        super();
-        addPerfil(Perfil.CLIENTE);
+
     }
 
-    public List<Chamados> getChamados() {
-        return chamados;
+
+    public void setPerfis(Perfil perfil){
+        this.perfis.add(perfil.getCodigo());
     }
 
-    public void setChamados(List<Chamados> chamados) {
-        this.chamados = chamados;
+    public Set<Perfil> getPerfis(){
+        return this.perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
     }
 }
