225bea3a4611cf143785d1cb410d01d448765f09
giovaner10 <giovanerr10@gmail.com>
2022-04-30 22:10:51 -0300

    tecnico controller

--- src/main/java/com/example/apiprojeto/controller/TecnicosController.java
+++ src/main/java/com/example/apiprojeto/controller/TecnicosController.java
@@ -1,14 +1,14 @@
 package com.example.apiprojeto.controller;
 
 import com.example.apiprojeto.domain.Tecnico;
+import com.example.apiprojeto.domain.dto.TecnicoDTO;
 import com.example.apiprojeto.service.TecnicosService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tecnicos")
@@ -19,7 +19,25 @@ public class TecnicosController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
-        return new ResponseEntity<>(tecnicosService.findById(id), HttpStatus.OK);
+
+
+        return new ResponseEntity(tecnicosService.findById(id), HttpStatus.OK);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Tecnico>> findAll(){
+
+
+        return new ResponseEntity<>(tecnicosService.findAll(), HttpStatus.OK);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Tecnico> save(@RequestBody Tecnico tecnico){
+
+
+        return new ResponseEntity(tecnicosService.save(tecnico), HttpStatus.CREATED);
 
     }
 }
