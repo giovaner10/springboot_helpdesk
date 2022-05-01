ea58511c142a26634d054de17bb43298064f08fb
giovaner10 <giovanerr10@gmail.com>
2022-04-30 22:11:21 -0300

    tecnicodto

---
+++ src/main/java/com/example/apiprojeto/domain/dto/TecnicoDTO.java
@@ -0,0 +1,101 @@
+package com.example.apiprojeto.domain.dto;
+
+import com.example.apiprojeto.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+public class TecnicoDTO {
+
+
+    private Integer id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String senha;
+    private Set<Integer> perfis = new HashSet<>();
+    private LocalDate dataCriacao = LocalDate.now();
+
+
+    public TecnicoDTO(Integer id, String nome, String cpf, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+
+    }
+
+    public TecnicoDTO() {
+
+    }
+
+
+    public void setPerfis(Perfil perfil){
+        this.perfis.add(perfil.getCodigo());
+    }
+
+    public Set<Perfil> getPerfis(){
+        return this.perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+}
+
