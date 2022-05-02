ae26dbebae911f7d46c4fa3556d5ab2a1a438c7f
giovaner10 <giovanerr10@gmail.com>
2022-05-01 23:44:07 -0300

    repo de chamados

--- src/main/java/com/example/apiprojeto/repository/ChamadoRepository.java
+++ src/main/java/com/example/apiprojeto/repository/ChamadoRepository.java
@@ -3,5 +3,5 @@ package com.example.apiprojeto.repository;
 import com.example.apiprojeto.domain.Chamados;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChamadoRepository extends JpaRepository<Chamados, Integer> {
+public interface ChamadoRepository extends JpaRepository<Chamados, Long> {
 }
