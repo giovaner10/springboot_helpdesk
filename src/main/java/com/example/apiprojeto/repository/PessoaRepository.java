4225dd1fa6954ef7f586d5dddfeacaf99ff1c190
Giovane A. Tiburcio <75172524+giovaner10@users.noreply.github.com>
2022-05-01 23:53:28 -0300

    Delete PessoaRepository.java

--- src/main/java/com/example/apiprojeto/repository/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.apiprojeto.repository;
-
-import com.example.apiprojeto.domain.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepository  extends JpaRepository<Pessoa, Integer> {
-}
