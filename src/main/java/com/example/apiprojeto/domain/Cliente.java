9cc7a37cf9c59834004ba432ca003b2573b0b549
giovaner10 <giovanerr10@gmail.com>
2022-05-01 23:44:55 -0300

    Cliente.java

--- src/main/java/com/example/apiprojeto/domain/Cliente.java
+++ src/main/java/com/example/apiprojeto/domain/Cliente.java
@@ -2,21 +2,33 @@ package com.example.apiprojeto.domain;
 
 import com.example.apiprojeto.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Entity
+@AllArgsConstructor
 public class Cliente {
 
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
+    @NotBlank
     private String nome;
+    @CPF
     private String cpf;
+    @Email
     private String email;
     private String senha;
     @ElementCollection(fetch = FetchType.EAGER)
@@ -24,17 +36,9 @@ public class Cliente {
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCriacao = LocalDate.now();
 
-
-
-    public Cliente(Integer id, String nome, String cpf, String email, String senha) {
-        this.id = id;
-        this.nome = nome;
-        this.cpf = cpf;
-        this.email = email;
-        this.senha = senha;
-        setPerfis(Perfil.CLIENTE);
-
-    }
+    @OneToMany(mappedBy = "cliente")
+    @JsonIgnore
+    private List<Chamados> chamados = new ArrayList<>();
 
     public Cliente() {
 
@@ -49,11 +53,11 @@ public class Cliente {
         return this.perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
