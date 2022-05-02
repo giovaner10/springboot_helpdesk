e0ee18960b75fb11f4fae427dd6a7c3b6947db30
giovaner10 <giovanerr10@gmail.com>
2022-05-01 23:48:26 -0300

    repositorio para tecnico

--- src/main/java/com/example/apiprojeto/repository/TecnicoRepository.java
+++ src/main/java/com/example/apiprojeto/repository/TecnicoRepository.java
@@ -3,5 +3,5 @@ package com.example.apiprojeto.repository;
 import com.example.apiprojeto.domain.Tecnico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TecnicoRepository extends JpaRepository<Tecnico, Integer> {
+public interface TecnicoRepository extends JpaRepository<Tecnico, Long> {
 }
