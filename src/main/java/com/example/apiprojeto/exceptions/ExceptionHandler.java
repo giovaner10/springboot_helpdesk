5bd674842ef961412b7b969a08b2dad6a0ff4a11
giovaner10 <giovanerr10@gmail.com>
2022-05-01 23:46:33 -0300

    tratando as validacoes de campos

--- src/main/java/com/example/apiprojeto/exceptions/ExceptionHandler.java
+++ src/main/java/com/example/apiprojeto/exceptions/ExceptionHandler.java
@@ -3,11 +3,15 @@ package com.example.apiprojeto.exceptions;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ExceptionHandler extends ResponseEntityExceptionHandler {
@@ -30,6 +34,30 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String detail = "O item que apresenta erros no seu corpo, verifique e tente novamenete";
+
+        BindingResult bindingResult = ex.getBindingResult();
+
+        List<BadRequestExceptionDetails.Field> problemField = bindingResult
+                .getFieldErrors()
+                .stream()
+                .map(fieldError ->
+
+                        BadRequestExceptionDetails.Field.builder()
+                            .name(fieldError.getField())
+                            .UserMesage(fieldError.getDefaultMessage())
+                            .build()
+                )
+                .collect(Collectors.toList());
+
+        BadRequestExceptionDetails exception = createException(status, ExceptionType.DADOS_INVALIDOS, detail)
+                .fields(problemField)
+                .build();
+
+        return handleExceptionInternal(ex,exception, new HttpHeaders(), status, request);
+    }
     public BadRequestExceptionDetails.BadRequestExceptionDetailsBuilder createException(HttpStatus status
             , ExceptionType exceptionType, String detail){
 
