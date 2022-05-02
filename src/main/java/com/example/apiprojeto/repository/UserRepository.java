1e0c262bc713ff523abc42b863386cc4ada1a68b
giovaner10 <giovanerr10@gmail.com>
2022-05-01 23:48:07 -0300

    repositorio para autenticacao em memoria

---
+++ src/main/java/com/example/apiprojeto/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.apiprojeto.repository;
+
+import com.example.apiprojeto.security.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+}
