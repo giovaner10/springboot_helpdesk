b5983e81455b21386b7a38802ca4ff9167834d22
giovaner10 <giovanerr10@gmail.com>
2022-05-01 23:45:14 -0300

    Cliente.java repo

--- src/main/java/com/example/apiprojeto/repository/ClienteRepository.java
+++ src/main/java/com/example/apiprojeto/repository/ClienteRepository.java
@@ -3,5 +3,5 @@ package com.example.apiprojeto.repository;
 import com.example.apiprojeto.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
