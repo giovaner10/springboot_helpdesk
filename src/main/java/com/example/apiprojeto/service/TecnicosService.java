5a47ec5af898dc9680f86eaac702544d469914b8
giovaner10 <giovanerr10@gmail.com>
2022-05-01 23:50:23 -0300

    TecnicosService.java

--- src/main/java/com/example/apiprojeto/service/TecnicosService.java
+++ src/main/java/com/example/apiprojeto/service/TecnicosService.java
@@ -6,6 +6,7 @@ import com.example.apiprojeto.domain.enums.Perfil;
 import com.example.apiprojeto.exceptions.ObjectNotFoundException;
 import com.example.apiprojeto.repository.TecnicoRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,8 +18,9 @@ public class TecnicosService {
 
     private final TecnicoRepository tecnicoRepository;
     private final TecnicoAssembler tecnicoAssembler;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public Optional<Tecnico> findById(Integer id) {
+    public Optional<Tecnico> findById(Long id) {
 
         return Optional.ofNullable(tecnicoRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Tecnico nao registrado")));
 
@@ -32,6 +34,22 @@ public class TecnicosService {
     public Tecnico save(Tecnico tecnico){
         tecnico.setPerfis(Perfil.CLIENTE);
         tecnico.setPerfis(Perfil.TECNICO);
+       String senha = tecnico.getSenha();
+        String encode = bCryptPasswordEncoder.encode(senha);
+        tecnico.setSenha(encode);
+
         return tecnicoRepository.save(tecnico);
+    }
+
+    public Tecnico update(Tecnico tecnico){
+        findById(tecnico.getId());
+
+        return save(tecnico);
+    }
+
+    public void delete(Long id){
+        findById(id);
+
+         tecnicoRepository.deleteById(id);
     }
 }
